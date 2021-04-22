9665a14c1949a0c0c48daf779d2d799b7f0f9dfe
NoahZinter <noah.zinter@gmail.com>
2021-04-22 14:53:00 -0600

    Add letters array

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,10 @@
 class Enigma
-
+  attr_reader :letters
   def initialize
+    @letters = ('a'..'z').to_a << ' '
+  end
+
+  def encrypt
+
   end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -7,5 +7,25 @@ describe Enigma do
       enigma = Enigma.new
       expect(enigma).is_a? Enigma
     end
+
+    it 'has a letters array' do
+      enigma = Enigma.new
+      expect(enigma.letters).to eq(["a", "b", "c",
+      "d", "e", "f", "g", "h", "i", "j", "k", "l",
+      "m", "n", "o", "p", "q", "r", "s", "t", "u",
+      "v", "w", "x", "y", "z", " "])
+    end
   end
+
+  # describe '#encrypt' do
+  #   it 'returns a hash of encryption, key, date' do
+  #     enigma = Enigma.new
+  #     expect(enigma.encrypt('hello world', '02715', '040895')).to eq (
+  #     {
+  #       encryption: "keder ohulw",
+  #       key: "02715",
+  #       date: "040895"
+  #     })
+  #   end
+  # end
 end
