c1347baefc90bfe4e2313fa3650795aaef258067
NoahZinter <noah.zinter@gmail.com>
2021-04-22 15:33:16 -0600

    Test #split_keys

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,11 +4,34 @@ class Enigma
     @letters = ('a'..'z').to_a << ' '
   end
 
-  def encrypt(string, key, date)
+  def encrypt(string, key = split_keys, date = Date.today)
     cipher = Hash.new
-    cipher[:encryption] = string
+    cipher[:encryption] = encode_string(string, key, date)
     cipher[:key] = key
     cipher[:date] = date
     cipher
+  end
+
+  def encode_string(string, key, date)
+
+  end
+
+  def generate_5
+    5.times.map{rand(5)}.join
+  end
+
+  def split_keys
+    total_key = generate_5.split('')
+    key_array = []
+    key_a = total_key[0..1].join
+    key_b = total_key[1..2].join
+    key_c = total_key[2..3].join
+    key_d = total_key[3..4].join
+    key_array.push(key_a,key_b,key_c,key_d)
+    key_array
+  end
+
+  def generate_offset(date)
+
   end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -17,9 +17,30 @@ describe Enigma do
     end
   end
 
-  describe '#encrypt' do
-    it 'returns a hash of encryption, key, date' do
+  describe '#generate_5' do
+    it 'generates a random 5 digit number' do
       enigma = Enigma.new
+      enigma.generate_5
+      enigma.split_keys
+      expect(enigma.generate_5).is_a? String
+      expect(enigma.generate_5.length).to eq 5
+    end
+  end
+
+  describe '#split_keys' do
+    it 'splits a 5 character string into 4 keys' do
+      enigma = Enigma.new
+      allow(enigma).to receive(:generate_5) { '12345' }
+
+      expect(enigma.split_keys).to eq (['12', '23', '34', '45'])
+    end
+  end
+
+
+  describe '#encrypt' do
+    xit 'returns a hash of encryption, key, date' do
+      enigma = Enigma.new
+      # allow(enigma).to_receive(:generate_key).and return(02715)
       expect(enigma.encrypt('hello world', '02715', '040895')).to eq (
       {
         encryption: "keder ohulw",
