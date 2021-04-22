a4010a5684825ad1036a595a3ebab7804b79d0e8
NoahZinter <noah.zinter@gmail.com>
2021-04-22 14:39:56 -0600

    Create Enigma, test it exists

---
+++ spec/enigma_spec.rb
@@ -0,0 +1,11 @@
+require 'date'
+require './lib/enigma'
+
+describe Enigma do
+  describe '#initialize' do
+    it 'exists' do
+      enigma = Enigma.new
+      expect(enigma).is_a? Enigma
+    end
+  end
+end
