c352c9fc016f4605d91b1755af6050d63fb49a7a
NoahZinter <noah.zinter@gmail.com>
2021-04-26 09:11:16 -0600

    Create encrypt and decrypt files, decrypt not working

---
+++ lib/decrypt.rb
@@ -0,0 +1,19 @@
+require './lib/enigma'
+require 'date'
+
+handle = File.open(ARGV[0], 'r')
+incoming_text = handle.read
+handle.close
+
+decrypt_handle = File.open(ARGV[1], 'w')
+
+key = ARGV[2].to_s
+date = ARGV[3].to_s
+
+enigma = Enigma.new
+decrypted_message = enigma.decrypt(incoming_text, key, date)
+
+puts "Created #{ARGV[1]} with key #{key} and date #{date}"
+
+decrypt_handle.write(decrypted_message)
+decrypt_handle.close
