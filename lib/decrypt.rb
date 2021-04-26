dde4490e1a092ea6fb9a3b88a099072503ea01c5
NoahZinter <noah.zinter@gmail.com>
2021-04-26 10:52:33 -0600

    Use eval to access incoming_text as hash

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,9 +1,12 @@
 require './lib/enigma'
 require 'date'
+require 'yaml'
 
 handle = File.open(ARGV[0], 'r')
 incoming_text = handle.read
 handle.close
+encrypted_hash = eval(incoming_text)
+encryption = encrypted_hash[:encryption]
 
 decrypt_handle = File.open(ARGV[1], 'w')
 
@@ -11,7 +14,7 @@ key = ARGV[2].to_s
 date = ARGV[3].to_s
 
 enigma = Enigma.new
-decrypted_message = enigma.decrypt(incoming_text, key, date)
+decrypted_message = enigma.decrypt(encryption, key, date)
 
 puts "Created #{ARGV[1]} with key #{key} and date #{date}"
 
